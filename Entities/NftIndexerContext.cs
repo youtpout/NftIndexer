b4aa8fefe649e769ae9d110d77ec6ba7fbcc735b
eddy <be@be-technology.fr>
2023-07-21 20:22:52 +0200

    init project

---
+++ Entities/NftIndexerContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace NftIndexer.Entities
+{
+	public class NftIndexerContext :DbContext
+	{
+		public NftIndexerContext()
+		{
+		}
+	}
+}
+
